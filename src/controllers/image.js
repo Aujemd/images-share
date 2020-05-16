a6b58e19b77eb84df060d97854ea22ef0acc4737
Johan Mora <joenmodu99@gmail.com>
2020-05-16 18:50:26 -0400

    Guardando imagenes en mongodb

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,23 +1,45 @@
 const path = require("path")
 const {random} = require("../helpers/libs")
 const fs = require("fs-extra")
+const {Image} = require("../models")
+
 const ctrl = {}
 
 ctrl.index = (req, res) => {
     res.send("Index Page")
 }
 
-ctrl.create = async (req, res) => {
-    const imgUrl = random()
-    const ext = path.extname(req.file.originalname).toLowerCase()
-    const imageTempPath = req.file.path
-    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`)
+ctrl.create = (req, res) => {
 
-    if(ext === ".png" || ext === ".jpg" || ext === ".gif"){
-        await fs.rename(imageTempPath, targetPath)
+    const saveImage = async () =>{
+        const imgUrl = random()
+        const images = await Image.find({filename: imgUrl})
+
+        if(images.length > 0){
+            saveImage()
+        }else{
+            const ext = path.extname(req.file.originalname).toLowerCase()
+            const imageTempPath = req.file.path
+            const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`)
+
+            if(ext === ".png" || ext === ".jpg" || ext === ".gif"){
+                await fs.rename(imageTempPath, targetPath)
+                const newImg = new Image({
+                    title: req.body.title,
+                    filename: imgUrl + ext,
+                    description: req.body.description
+                })
+                const imageSaved = await newImg.save()
+                res.send("Works!")
+            }else{
+                await fs.unlink(imageTempPath)
+                res.status(500).json({error: "Only Images are allowed"})
+            }
+        }
     }
-    res.send("Works!")
-}
+
+    saveImage()
+    }
 
 ctrl.like = (req, res) => {
     res.send("Like Page")
