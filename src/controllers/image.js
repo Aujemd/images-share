65325800e942048f03dc6333875a1079072c232a
Johan Mora <joenmodu99@gmail.com>
2020-05-18 21:13:07 -0400

    Actualizando las vistas

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -7,9 +7,20 @@ const md5 = require("md5")
 const ctrl = {}
 
 ctrl.index = async (req, res) => {
+    const viewModel = {image: {}, comments: {}}
     const image = await Image.findOne({filename: {$regex: req.params.image_id}})
-    const comments = await Comment.find({image_id: image._id})
-    res.render("image", {image, comments})
+    if(image){
+        image.views += 1
+        viewModel.image = image
+        await image.save()
+        const comments = await Comment.find({image_id: image._id})
+        viewModel.comments = comments
+        res.render("image", viewModel)
+    }else{
+        res.redirect("/")
+    }
+
+
 }
 
 ctrl.create = (req, res) => {
@@ -56,8 +67,9 @@ ctrl.comment = async (req, res) => {
         newComment.image_id = image._id
         await newComment.save()
         res.redirect("/images/" + image.uniqueId)
+    }else{
+        res.redirect("/")
     }
-    res.send("Comment Page")
 }
 
 ctrl.remove = (req, res) => {
